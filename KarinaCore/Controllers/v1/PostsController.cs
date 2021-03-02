d0301b85d86a4ab46662c1ed581b6a5182e5e080
Dino <elvedingran@gmail.com>
2021-03-02 15:33:35 +0100

    Initial commit

---
+++ KarinaCore/Controllers/v1/PostsController.cs
@@ -0,0 +1,31 @@
+﻿using FuckCore.Contracts;
+using FuckCore.Contracts.v1;
+using FuckCore.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FuckCore.Controllers.v1
+{
+    public class PostsController : Controller
+    {
+        private List<Post> _posts;
+
+        public PostsController()
+        {
+            _posts = new List<Post>();
+            for (var i = 0; i < 5; i++)
+            {
+                _posts.Add(new Post { Id = Guid.NewGuid().ToString() });
+            }
+        }
+        [HttpGet(Contracts.ApiRoutes.Posts.GetAll)]
+
+        public IActionResult GetAll()
+        {
+            return Ok(_posts);
+        }
+    }
+}
---
+++ KarinaCore/Domain/Post.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FuckCore.Domain
+{
+   public class Post
+    {
+        public string Id { get; set; }
+    }
+}
