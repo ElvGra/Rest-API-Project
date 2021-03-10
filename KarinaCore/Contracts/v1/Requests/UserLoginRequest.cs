8be045b431ad16f5a0ea2f32a2576c0fa1a35709
Dino <elvedingran@gmail.com>
2021-03-10 18:39:08 +0100

    Added login

--- KarinaCore/Contracts/v1/Requests/UserLoginRequest.cs
+++ KarinaCore/Contracts/v1/Requests/UserLoginRequest.cs
@@ -1,16 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FuckCore.Contracts.v1.Requests
 {
-    public class UserRegistrationRequest
+    public class UserLoginRequest
     {
-        [EmailAddress]
         public string Email { get; set; }
-        
+
         public string Password { get; set; }
     }
 }
--- KarinaCore/Services/IdentityService.cs
+++ KarinaCore/Services/IdentityService.cs
@@ -28,7 +28,7 @@ namespace FuckCore.Services
         {
             var existingUser = await _userManager.FindByEmailAsync(email);
 
-            if(existingUser != null)
+            if (existingUser != null)
             {
                 return new AuthenticationResult
                 {
@@ -52,6 +52,38 @@ namespace FuckCore.Services
                 };
             }
 
+            return GenerateAuthenticationResultForUser(newUser);
+        }
+
+        public async Task<AuthenticationResult> LoginAsync(string email, string password)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if (user == null)
+            {
+                return new AuthenticationResult
+                {
+                    Errors = new[] { "User does not exist" }
+                };
+            }
+
+            var userHasValidPassword = await _userManager.CheckPasswordAsync(user, password);
+
+            if (!userHasValidPassword)
+            {
+                return new AuthenticationResult
+                {
+                    Errors = new[] { "User/password combination is wrong" }
+                };
+
+            }
+
+            return GenerateAuthenticationResultForUser(user);
+
+        }
+
+        private AuthenticationResult GenerateAuthenticationResultForUser(IdentityUser newUser)
+        {
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
             var tokenDescriptor = new SecurityTokenDescriptor
@@ -75,6 +107,5 @@ namespace FuckCore.Services
                 Token = tokenHandler.WriteToken(token)
             };
         }
-
     }
 }
